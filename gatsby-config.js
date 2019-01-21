e2d8155ef0db0ffa6d6679c853b17c69f6e8fc3b
dev <sheyiadekoya@gmail.com>
2019-01-21 23:58:40 +0100

    add google tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-129125077-1`,
       },
     },
     `gatsby-plugin-feed`,
